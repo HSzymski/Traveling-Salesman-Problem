23e5106acfeeb48c98a135d0a048510dbbe9ddf7
Hubert Szymski <hubertszymski@gmail.com>
2021-07-22 21:30:23 +0200

    First TSP solution commit

---
+++ Ant_System.py
@@ -0,0 +1,172 @@
+import numpy as np
+import pandas as pd
+import random
+from scipy.spatial import distance
+
+def ant_system(cities, k, num_of_tours, alpha, beta, p):
+    num_of_cities = cities.shape[0]
+
+    # calculate start level of the pheromone
+    array_of_city_dist = calc_distance_array(cities)
+    max_dist = np.nanmax(array_of_city_dist,axis=1)
+    tau_0 = 1/max_dist
+
+    # create list of ants which will move through the cities
+    list_of_ants = create_ants(k,num_of_cities)
+
+    # initialization of decision table
+    array_of_base_pheromone = np.zeros((num_of_cities,num_of_cities))
+    array_of_pheromone = np.copy(array_of_base_pheromone)
+    for i in range(num_of_cities):
+        for j in range(num_of_cities):
+            if(i==j):
+                array_of_base_pheromone[i][j] = np.nan
+            else:
+                array_of_base_pheromone[i][j] = tau_0[j]
+
+    # in the first tour choose paths randomly
+    for ant in list_of_ants:
+        for i in range(num_of_cities-1):
+            # draw city number
+            city_idx = random.choice(ant['cities_left'])
+
+            # save information about the traveled route
+            ant['cities_left'].remove(city_idx)
+            ant['dist_traveled'] += distance.euclidean(cities[ant['path'][-1]], cities[city_idx])
+            ant['path'].append(city_idx)
+
+        # after visiting each city return to the starting place
+        ant['dist_traveled'] += distance.euclidean(cities[ant['path'][-1]], cities[ant['start_point']])
+        ant['path'].append(ant['start_point'])
+
+        # sum up pheromone on each arc from this tour
+        for first_city_idx, second_city_idx in \
+                zip(ant['path'][:-1],ant['path'][1:]):
+            array_of_pheromone[first_city_idx][second_city_idx] += 1 / ant['dist_traveled']
+
+    # calculate level of pheromone between cities
+    array_of_whole_pheromone = p*array_of_base_pheromone+array_of_pheromone
+
+    # main loop of the algorithm
+    for tour in range(num_of_tours):
+        #list_of_ants = create_ants(k, num_of_cities)
+        list_of_ants = restart_ants(list_of_ants, num_of_cities)
+
+        for ant in list_of_ants:
+            for i in range(num_of_cities-1):
+                actual_city_idx = ant['path'][-1]
+                semi_decision_table = np.zeros(shape=(num_of_cities,1))
+                decision_table = np.zeros_like(semi_decision_table)
+
+                for city_idx in range(num_of_cities):
+                    # avoid same and visited cities
+                    if city_idx == actual_city_idx or city_idx not in ant['cities_left']:
+                        semi_decision_table[city_idx] = np.nan
+                    else:
+                        semi_decision_table[city_idx] = (array_of_whole_pheromone[actual_city_idx][city_idx]**\
+                                                    alpha) * ((1/array_of_city_dist[actual_city_idx][city_idx])**beta)
+                for j in range(num_of_cities):
+                    decision_table[j] = semi_decision_table[j]/\
+                                        np.nansum(semi_decision_table)
+                                        #np.nansum(decision_table[np.arange(len(decision_table))!=i])
+
+                probabilities_table = np.zeros_like(decision_table)
+                for k in range(num_of_cities):
+                    if not np.isnan(probabilities_table[k]):
+                        probabilities_table[k] = decision_table[k]/np.nansum(decision_table)
+
+                city_to_go_idx = where_to_go(probabilities_table)
+
+                # save information about the traveled route
+                ant['cities_left'].remove(city_to_go_idx)
+                ant['dist_traveled'] += distance.euclidean(cities[ant['path'][-1]], cities[city_to_go_idx])
+                ant['path'].append(city_to_go_idx)
+
+            # after visiting each city return to the starting place
+            ant['dist_traveled'] += distance.euclidean(cities[ant['path'][-1]], cities[ant['start_point']])
+            ant['path'].append(ant['start_point'])
+
+            # sum up pheromone on each arc from this tour
+            for first_city_idx, second_city_idx in \
+                    zip(ant['path'][:-1], ant['path'][1:]):
+                array_of_pheromone[first_city_idx][second_city_idx] += 1 / ant['dist_traveled']
+
+        array_of_whole_pheromone = p*array_of_whole_pheromone+array_of_pheromone
+
+    list_of_ants = sorted(list_of_ants, key=lambda ant: ant['dist_traveled'])
+    best_dist = list_of_ants[0]['dist_traveled']
+    return best_dist
+
+def create_ants(num_of_ants, num_of_cities):
+    list_of_ants = []
+    for i in range(num_of_ants):
+        rand_start_point = np.random.randint(0,num_of_cities-1)
+        start_dist = 0
+        ant = {"start_point":rand_start_point,"dist_traveled":start_dist,
+               "cities_left":list(range(num_of_cities)), "path":[rand_start_point]}
+        ant['cities_left'].remove(ant['start_point'])
+        list_of_ants.append(ant)
+    return list_of_ants
+
+
+def restart_ants(list_of_ants, num_of_cities):
+    for ant in list_of_ants:
+        ant['dist_traveled'] = 0
+        ant["cities_left"] = list(range(num_of_cities))
+        ant['cities_left'].remove(ant['start_point'])
+        ant['path'] = [ant['start_point']]
+    return list_of_ants
+
+
+def where_to_go(probabilities_table):
+    rand_num = np.random.uniform(0,1)
+    sum_of_probabilities = 0
+    index_of_city = -1
+    for idx, val in enumerate(probabilities_table.reshape((-1,))):
+        if not np.isnan(val):
+            sum_of_probabilities += val
+        if sum_of_probabilities >= rand_num:
+            index_of_city = idx
+            break
+    if index_of_city == -1:
+        for i in range(probabilities_table.shape[0]):
+            if np.isnan(probabilities_table[-1-i]):
+                continue
+            else:
+                index_of_city = i
+    return index_of_city
+
+
+def calc_distance_array(cities):
+    num_of_cities = cities.shape[0]
+    array_of_city_dist = np.zeros((num_of_cities, num_of_cities))
+    for i in range(num_of_cities):
+        for j in range(num_of_cities):
+            if(i==j):
+                array_of_city_dist[i][j] = np.nan
+            else:
+                city_dist = distance.euclidean(cities[i], cities[j])
+                array_of_city_dist[i][j] = city_dist
+                array_of_city_dist[j][i] = city_dist
+    return array_of_city_dist
+
+
+def main():
+    # reading data
+    cities_file = np.loadtxt("cities_4.txt").T
+
+    # num of: ants/ cities
+    k = cities_file.shape[0]
+    # control parameters
+    alpha = 1
+    beta = 5
+    # pheromone evaporation coefficient
+    p = 0.5
+    # number of iterations
+    iterations = 200
+
+    best_score = ant_system(cities_file, k, iterations, alpha, beta, p)
+    print(best_score)
+
+if __name__ == '__main__':
+    main()
