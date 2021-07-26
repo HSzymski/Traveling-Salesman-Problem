1616df2ef0b4d1dbc6fbe3d9163d66c6b59b6467
Hubert Szymski <hubertszymski@gmail.com>
2021-07-26 13:56:34 +0200

    Algorithm skeleton

--- Genetic_Algorithm.py
+++ Genetic_Algorithm.py
@@ -1,9 +1,24 @@
 import numpy
 
 
-def genetic_algorithm(cities_file: np.array) -> int:
+def genetic_algorithm(cities_file: np.array,
+                      p: int,
+                      num_of_iter: int,
+                      n: float,
+                      mutation_probability: float) -> int:
     create_initial_population()
-    evaluate_cost()
+
+    while i in range(num_of_iter):
+        evaluate_cost()
+        create_parent_population()
+
+        offspring_array_size = 0
+        while offspring_array_size == n*p:
+            create_offspring()
+
+
+        create_parent_population()
+
     pass
 
 
@@ -15,20 +30,29 @@ def evaluate_cost():
     pass
 
 
+def mutaation():
+    pass
+
+
+def create_offspring():
+    mutation()
+    pass
+
+
 def main():
     # reading data
     cities_file = np.loadtxt("Data\cities_4.txt").T
 
     # population size
     p = 250
-    # offspring to parent population size ratio
+    # parent to population population size ratio
     n = 0.8
     # probability of mutation
     mutation_probability = 0.2
     # number of iterations
     num_of_iter = 1000
 
-    best_score = genetic_algorithm(cities_file)
+    best_score = genetic_algorithm(cities_file, p, num_of_iter, n , mutation_probability)
     print(best_score)
 
 if __name__ == '__main__':
