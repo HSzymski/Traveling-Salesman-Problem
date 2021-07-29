d6ab1904bfb40e1f7ab92ce8f59a76bb8c0d7589
Hubert Szymski <hubertszymski@gmail.com>
2021-07-29 21:25:52 +0200

    Adding plots, printing best results and measure times of execution

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import numpy as np
+import time
 from ant_system import ant_system
 from genetic_algorithm import genetic_algorithm
+from matplotlib import pyplot as plt
 
 
 def main():
@@ -15,15 +17,22 @@ def main():
     # pheromone evaporation coefficient
     p = 0.5
     # number of iterations
-    num_of_iter = 200
+    num_of_iter = 250
 
-    best_score, best_path = ant_system(cities_file,
-                                       k,
-                                       num_of_iter,
-                                       alpha,
-                                       beta,
-                                       p)
-    print("Ant system: ", best_score, best_path)
+    start_as = time.time()
+    best_score_as, best_path_as = ant_system(cities_file,
+                                             k,
+                                             num_of_iter,
+                                             alpha,
+                                             beta,
+                                             p)
+    end_as = time.time()
+    print("Ant system \nscore: ", best_score_as, "\nbest path: ", best_path_as, "\ntime of execution: ",
+          end_as - start_as)
+
+    cities_as = np.ones((k+1, 2))*(-1)
+    for idx, val in enumerate(best_path_as):
+        cities_as[idx] = cities_file[val]
 
     # population size
     population_size = 250
@@ -36,13 +45,28 @@ def main():
     # algorithm used to select parents from population
     selection = "roulette_wheel"
 
-    best_score, best_path = genetic_algorithm(cities_file,
-                                              population_size,
-                                              num_of_iter,
-                                              n,
-                                              mutation_probability,
-                                              selection)
-    print("Genetic algorithm: ", best_score, best_path)
+    start_ga = time.time()
+    best_score_ga, best_path_ga = genetic_algorithm(cities_file,
+                                                    population_size,
+                                                    num_of_iter,
+                                                    n,
+                                                    mutation_probability,
+                                                    selection)
+    end_ga = time.time()
+    print("Genetic algorithm \nscore: ", best_score_ga, "\nbest path: ", best_path_ga, "\ntime of execution: ",
+          end_ga - start_ga)
+    cities_ga = np.ones((k+1, 2))*(-1)
+    for idx, val in enumerate(best_path_ga):
+        cities_ga[idx] = cities_file[val]
+
+    fig, ax = plt.subplots(1, 2)
+    ax[0].set_title("Path for Ant System")
+    ax[1].set_title("Path for Genetic Algorithm")
+    ax[0].plot(cities_as[:, 0], cities_as[:, 1])
+    ax[0].scatter(cities_as[:, 0], cities_as[:, 1], c='r')
+    ax[1].plot(cities_ga[:, 0], cities_ga[:, 1])
+    ax[1].scatter(cities_ga[:, 0], cities_ga[:, 1], c='r')
+    plt.show()
 
 
 if __name__ == '__main__':
